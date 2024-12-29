73ff21f4b48f27c6906f64105712f00d72c56201
Ganesh <ganeshpatelchetti@gmail.com>
2024-12-30 00:41:05 +0530

    bug sloved on iterations

--- src/User/Userlistedjobs.jsx
+++ src/User/Userlistedjobs.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 import {
   AppBar,
@@ -22,7 +21,7 @@ import {
   Alert,
 } from "@mui/material";
 import { AccountCircle } from "@mui/icons-material";
-import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import Footer from "../components/footer";
@@ -41,23 +40,23 @@ const Userlistedjobs = () => {
     const fetchData = async () => {
       try {
         // Retrieve the token from local storage
-        const token = localStorage.getItem('token');
-        
+        const token = localStorage.getItem("token");
+
         const response = await axios.get(
           `https://my-elegant-backend-api.onrender.com/jobs/getJobsByCategory?category=${category}`,
           {
             headers: {
-              'Authorization': `Bearer ${token}` // Include the token in the request
-            }
+              Authorization: `Bearer ${token}`, // Include the token in the request
+            },
           }
         );
         setData(response.data.data);
       } catch (err) {
-        console.error('Error fetching data:', err);
+        console.error("Error fetching data:", err);
         if (err.response && err.response.status === 403) {
-          setError('Unauthorized access. Please log in.');
+          setError("Unauthorized access. Please log in.");
         } else {
-          setError('Unable to load data. Please try again later.');
+          setError("Unable to load data. Please try again later.");
         }
       } finally {
         setLoading(false);
@@ -84,7 +83,7 @@ const Userlistedjobs = () => {
 
   const handleJobClick = (job) => {
     setSelectedJob(job);
-    console.log("job",job._id)
+    console.log("job", job._id);
   };
 
   const handleApply = async () => {
@@ -94,13 +93,13 @@ const Userlistedjobs = () => {
         navigate("/Userlogin");
         return;
       }
-      
+
       const response = await axios.get(
-        `https://my-elegant-backend-api.onrender.com/application/user-apply/${selectedJob._id}` ,
+        `https://my-elegant-backend-api.onrender.com/application/user-apply/${selectedJob._id}`,
         {
-          headers: { 
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
           },
         }
       );
@@ -111,7 +110,7 @@ const Userlistedjobs = () => {
       console.error("Error applying for the job:", err);
       setApplySuccess(false);
     } finally {
-      setSelectedJob(null); 
+      setSelectedJob(null);
     }
   };
 
@@ -122,9 +121,20 @@ const Userlistedjobs = () => {
   return (
     <Box>
       {/* Navbar */}
-      <AppBar position="sticky" color="primary" sx={{background: "rgb(228, 45, 64)"}} >
+      <AppBar
+        position="sticky"
+        color="primary"
+        sx={{ background: "rgb(228, 45, 64)" }}
+      >
         <Toolbar>
-          <Typography variant="h4" sx={{ flexGrow: 1, textAlign: "left",fontFamily: "Times New Roman", }}>
+          <Typography
+            variant="h4"
+            sx={{
+              flexGrow: 1,
+              textAlign: "left",
+              fontFamily: "Times New Roman",
+            }}
+          >
             RealEstatePro
           </Typography>
 
@@ -160,7 +170,12 @@ const Userlistedjobs = () => {
 
       {/* Page Content */}
       <Container sx={{ my: 4 }}>
-        <Typography variant="h4" gutterBottom textAlign="center" sx={{animation: "colorChange 3s infinite",}}>
+        <Typography
+          variant="h4"
+          gutterBottom
+          textAlign="center"
+          sx={{ animation: "colorChange 3s infinite" }}
+        >
           {category} Jobs
         </Typography>
         <style>
@@ -184,39 +199,46 @@ const Userlistedjobs = () => {
           </Typography>
         ) : data.length > 0 ? (
           <Grid container spacing={4} justifyContent="center">
-            {data.map((item, index) => (
-              // console.log(item.jobs[index]),
-              <Grid item xs={12} sm={6} md={4} key={index}>
-                <Card
-                  onClick={() => handleJobClick(item.jobs[index])}
-                  sx={{
-                    cursor: "pointer",
-                    backgroundColor: "#dab6ff",
-                    transition: "transform 0.3s, box-shadow 0.3s",
-                    "&:hover": {
-                      transform: "scale(1.05)",
-                      boxShadow: 6,
-                      backgroundColor: "#f3e9ff"
-                    },
-                  }}
+            {data.map((item, dataIndex) =>
+              item.jobs.map((job, jobIndex) => (
+                <Grid
+                  item
+                  xs={12}
+                  sm={6}
+                  md={4}
+                  key={`${dataIndex}-${jobIndex}`}
                 >
-                  <CardContent>
-                    <Typography variant="h4" gutterBottom>
-                      {item.jobs[index].title}
-                    </Typography>
-                    <Typography variant="h6" gutterBottom>
-                      {item.jobs[index].company.companyName}
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary">
-                      {item.jobs[index].location}
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary">
-                      {item.jobs[index].salary}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
+                  <Card
+                    onClick={() => handleJobClick(job)}
+                    sx={{
+                      cursor: "pointer",
+                      backgroundColor: "#dab6ff",
+                      transition: "transform 0.3s, box-shadow 0.3s",
+                      "&:hover": {
+                        transform: "scale(1.05)",
+                        boxShadow: 6,
+                        backgroundColor: "#f3e9ff",
+                      },
+                    }}
+                  >
+                    <CardContent>
+                      <Typography variant="h4" gutterBottom>
+                        {job.title}
+                      </Typography>
+                      <Typography variant="h6" gutterBottom>
+                        {job.company.companyName}
+                      </Typography>
+                      <Typography variant="body1" color="textSecondary">
+                        {job.location}
+                      </Typography>
+                      <Typography variant="body1" color="textSecondary">
+                        {job.salary}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))
+            )}
           </Grid>
         ) : (
           <Typography variant="body1" textAlign="center">
@@ -227,7 +249,7 @@ const Userlistedjobs = () => {
 
       {/* Job Details Dialog */}
       {selectedJob && (
-        <Dialog open={Boolean(selectedJob)} onClose={handleCancel} >
+        <Dialog open={Boolean(selectedJob)} onClose={handleCancel}>
           <DialogTitle>{selectedJob.category} Job Details</DialogTitle>
           <DialogContent>
             <Typography variant="body1" gutterBottom>
@@ -265,7 +287,12 @@ const Userlistedjobs = () => {
             </Typography>
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleApply} sx={{backgroundColor:"GrayText",color:"#fff"}}>Apply</Button>
+            <Button
+              onClick={handleApply}
+              sx={{ backgroundColor: "GrayText", color: "#fff" }}
+            >
+              Apply
+            </Button>
             <Snackbar
               open={applySuccess}
               autoHideDuration={3000} // Auto-hide after 3 seconds
@@ -295,7 +322,7 @@ const Userlistedjobs = () => {
           </Alert>
         </Snackbar>
       )}
-      <Footer/>
+      <Footer />
     </Box>
   );
 };
